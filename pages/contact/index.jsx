a01fffdfdfaa958b80bdbda914f69139d2942b91
Jon Tornetta <jmtornetta@gmail.com>
2022-07-27 13:04:57 -0400

    Feature: Add calendar and contact form components to contact page

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,10 +1,15 @@
 import Layout from "/components/layout";
 import ContactForm from "/components/contactForm";
+import Calendar from "/components/calendar";
 
 export default function Contact() {
     return (
         <Layout title="Contact">
-            <ContactForm />
+            {/* Make max container width just large enough so that inner calendar hits its max width. This keeps the contact form and calendar widths equal. */}
+            <div className="container max-w-[865px] mx-auto">
+                <ContactForm />
+                <Calendar />
+            </div>
         </Layout>
     )
 }
